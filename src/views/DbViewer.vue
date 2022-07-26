aea9b247bb125ba4abdb4917c173db26f0bd4886
Radoslav <56716736+Radi-AT@users.noreply.github.com>
2022-07-27 01:15:00 +0200

    Conexion establecida con firestore lite

--- src/views/DbViewer.vue
+++ src/views/DbViewer.vue
@@ -21,25 +21,51 @@ onBeforeUnmount(() => {
 });
 
 onMounted(() => {
-  getAnillos(db);
+  getRings(db);
 });
 
-async function getAnillos(db) {
+async function getRings(db) {
   const querySnapshot = await getDocs(collection(db, 'anillos'));
+  let ringList = [];
   querySnapshot.forEach(doc => {
     console.log(doc.id, doc.data());
-    dataBase = doc.id, doc.data().nombre;
+    ringList.push({
+      id: doc.id,
+      name: doc.data().nombre,
+      lvl: doc.data().nivel,
+      lore: doc.data().lore,
+      skill: doc.data().poder,
+      price: doc.data().precio,
+      property: doc.data().propiedad,
+      slot: doc.data().slot,
+    });
   });
+
+  dataBase.value = ringList;
 }
 </script>
 
 <template>
     <section>
         <h1>Base de datos</h1>
-        <p>{{ dataBase }}</p>
+        <div v-for="item in dataBase" :key="item.id" class="item">
+          <p>ID: {{ item.id }}</p>
+          <p>NOMBRE: {{ item.name }}</p>
+          <p>NIVEL: {{ item.lvl }}</p>
+          <p>LORE: {{ item.lore }}</p>
+          <p>PODER: {{ item.skill }}</p>
+          <p>PRECIO: {{ item.price }}</p>
+          <p>PROPIEDAD: {{ item.property }}</p>
+          <p>SLOT: {{ item.slot }}</p>
+        </div>
     </section>
 </template>
 
-<style>
-
+<style lang="scss" scoped>
+.item {
+  border: 5px solid peru;
+  max-width: 500px;
+  margin: 0 auto 20px auto;
+  padding: 20px;
+}
 </style>
